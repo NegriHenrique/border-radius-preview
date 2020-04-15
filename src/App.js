ddf2ccfd3e8f348b592881bbbe79f72156222a8c
Henrique Negri <henriquenegri@gmail.com>
2020-04-15 12:04:58 -0300

    finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { FaCopy } from "react-icons/fa";
+
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function App() {
+  const [topLeft, setTopLeft] = useState(0);
+  const [topRight, setTopRight] = useState(0);
+  const [bottomLeft, setBottomLeft] = useState(0);
+  const [bottomRight, setBottomRight] = useState(0);
+  const [styleCssBorderRadius, setStyleCssBorderRadius] = useState("");
+  const [styleCssWebkit, setStyleCssWebkit] = useState("");
+  const [styleCssMoz, setStyleCssMoz] = useState("");
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    setStyleCssBorderRadius(
+      `border: ${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft};`
+    );
+    setStyleCssWebkit(
+      `-webkit-border: ${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft};`
+    );
+    setStyleCssMoz(
+      `-moz-border: ${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft};`
+    );
+  }, [topLeft, topRight, bottomRight, bottomLeft]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Border Radius Preview</h1>
+      <div className="box-wrapper">
+        <div
+          className="box"
+          style={{
+            borderTopLeftRadius: `${topLeft}px`,
+            borderTopRightRadius: `${topRight}px`,
+            borderBottomLeftRadius: `${bottomLeft}px`,
+            borderBottomRightRadius: `${bottomRight}px`
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <p>{styleCssBorderRadius}</p>
+          <p>{styleCssWebkit}</p>
+          <p>{styleCssMoz}</p>
+        </div>
+        <input
+          type="text"
+          className="top-left"
+          title="top-left"
+          value={topLeft}
+          onChange={e => setTopLeft(e.target.value)}
+        />
+        <input
+          type="text"
+          className="top-right"
+          title="top-right"
+          value={topRight}
+          onChange={e => setTopRight(e.target.value)}
+        />
+        <input
+          type="text"
+          className="bottom-left"
+          title="bottom-left"
+          value={bottomLeft}
+          onChange={e => setBottomLeft(e.target.value)}
+        />
+        <input
+          type="text"
+          className="bottom-right"
+          title="bottom-right"
+          value={bottomRight}
+          onChange={e => setBottomRight(e.target.value)}
+        />
+        <CopyToClipboard
+          text={`${styleCssBorderRadius} \n ${styleCssWebkit} \n ${styleCssMoz}`}
+          onCopy={() => {
+            setCopied(true);
+            setTimeout(() => {
+              setCopied(false);
+            }, 2000);
+          }}
+          className="copy-to-clipboard"
+        >
+          <span>
+            Copy to clipboard <FaCopy size={15} />
+            {copied && <span className="copied"> Copied! </span>}
+          </span>
+        </CopyToClipboard>
+      </div>
     </div>
   );
 }
